6b58fbd2fb3ccac3f4d3005b7fe5f73cde539eef
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-09 17:20:48 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -70,7 +70,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"© [{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"© [{get_size(file.file_size)}] {file.file_name}", callback_data=f'© files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -79,11 +79,11 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"© {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"© {file.file_name}", callback_data=f'© files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"© {get_size(file.file_size)}",
-                    callback_data=f'files_#{file.file_id}',
+                    callback_data=f'© files_#{file.file_id}',
                 ),
             ]
             for file in files
