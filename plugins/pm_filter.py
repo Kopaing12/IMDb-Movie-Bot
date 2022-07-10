ecdb2d07ddc232a7c60b8e85846367b3b50025da
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-10 21:32:06 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -89,27 +89,27 @@ async def next_page(bot, query):
             for file in files
         ]
 
-    if 0 < offset <= 10:
+    if 0 < offset <= 5:
         off_set = 0
     elif offset == 0:
         off_set = None
     else:
-        off_set = offset - 10
+        off_set = offset - 5
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton("『BACK』", callback_data=f"next_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"🔰 {round(int(offset) / 10) + 1} / {round(total / 10)}",
+             InlineKeyboardButton(f"🔰 {round(int(offset) / 5) + 1} / {round(total / 5)}",
              callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton(f"🔰{round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
+            [InlineKeyboardButton(f"🔰{round(int(offset) / 5) + 1} / {round(total / 5)}", callback_data="pages"),
              InlineKeyboardButton("『NEXT』", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
                 InlineKeyboardButton("⏪『BACK』", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"🔰 {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
+                InlineKeyboardButton(f"🔰 {round(int(offset) / 5) + 1} / {round(total / 5)}", callback_data="pages"),
                 InlineKeyboardButton("『NEXT』⏩", callback_data=f"next_{req}_{key}_{n_offset}")]
             )
     btn.insert(0,
@@ -937,7 +937,7 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"🔰 1/{round(int(total_results) / 10)}", callback_data="pages"),
+            [InlineKeyboardButton(text=f"🔰 1/{round(int(total_results) / 5)}", callback_data="pages"),
              InlineKeyboardButton(text="『 NEXT 』⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
